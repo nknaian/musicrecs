5f88d927887758fa6836746ca97a05cfc3eae5f4
bricknj <66092948+bricknj@users.noreply.github.com>
2021-01-19 23:11:22 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-export SPOTIPY_CLIENT_ID="1ae20820bae7472bbe6ed720871c6920"
-export SPOTIPY_CLIENT_SECRET="ade0fb611b4647baa1f6624409bb6b1a"
-export SQLALCHEMY_DATABASE_URI="sqlite:////home/nick/repos/musicrecs/database.db"
-export FLASK_SECRET_KEY="MEYLphB5nZSkumyYgF-P6w"
-export FLASK_APP=musicrecs
-export FLASK_ENV=development
