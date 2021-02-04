f4f66c218fb10d3c3782bd4dbbd6d63660ccbe63
Nick Knaian <nickknaian@gmail.com>
2021-02-04 16:16:58 -0500

    Remove test infrastructure for spotify user iface
    Refer to 0d4db49c536ec51fac1f8b23127956cc48cd2cd4 for a basic reference.
    
    Issue #2 is completed with this commit

--- musicrecs/views.py
+++ musicrecs/views.py
@@ -14,7 +14,7 @@ from musicrecs import spotify_iface
 from musicrecs.sql_models import Submission, Round
 from musicrecs.forms import NewRoundForm, TrackrecForm, AlbumrecForm
 from musicrecs.enums import RoundStatus, MusicType
-from musicrecs.exceptions import UserError, InternalError, SpotifyUserNotAuthenticated
+from musicrecs.exceptions import UserError, InternalError
 from musicrecs.spotify import spotify_user
 
 
@@ -184,24 +184,6 @@ def sp_login_success():
     if request.args.get("code"):
         spotify_user.auth_new_user(request.args.get("code"))
     return redirect(url_for('sp'))
-
-
-@app.route('/sp', methods=["GET", "POST"])
-def sp():
-    if 'sp_current_track' in request.form:
-        try:
-            print(spotify_user.get_current_track())
-        except SpotifyUserNotAuthenticated as e:
-            return redirect(e.get_auth_url())
-    elif 'sp_signout' in request.form:
-        return redirect(spotify_user.not_you())
-
-    try:
-        display_name = spotify_user.get_user_display_name()
-    except SpotifyUserNotAuthenticated:
-        display_name = None
-
-    return render_template('sp/sp.html', display_name=display_name)
 
 
 '''PRIVATE FUNCTIONS'''
